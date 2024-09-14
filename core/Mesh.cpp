c18c1ae9a8688515589ca5cc4cd0d84908861b78
M-hypnos <46706215+M-hypnos@users.noreply.github.com>
2024-09-14 14:22:46 +0800

    Delete core directory

--- core/Mesh.cpp
+++
@@ -1,9 +0,0 @@
-#include "Mesh.h"
-
-using namespace std;
-
-Mesh::Mesh(vector<Vertex> vertices, vector<size_t> indices, vector<int> textureIdx) {
-	this->vertices = vertices;
-	this->indices = indices;
-	this->textureIdx = textureIdx;
-}
