18db4d5baa3c9121fc4a4d523fff855580770712
M-hypnos <46706215+M-hypnos@users.noreply.github.com>
2024-09-25 15:36:28 +0800

    按钮添加

--- entity/Scene.cpp
+++ entity/Scene.cpp
@@ -1,6 +1,7 @@
 #include "Scene.h"
-#include "../shader/TestShader.h"
+#include "../shader/TexShader.h"
 #include "../shader/LightShader.h"
+#include "../shader/WhiteShader.h"
 #include "../core/Pipeline.h"
 #include "../entity/QuatObject.h"
 
@@ -9,17 +10,41 @@ Vec3f cameraFront = Vec3f(0, 0, -1);
 Vec3f cameraUp = Vec3f(0, 1, 0);
 float cameraFov = 45.f;
 
-Scene::Scene(float aspectRatio) {
+Scene::Scene(float aspectRatio, SDL_Renderer* renderer) {
 	camera_ = new Camera(cameraEye, cameraFront, cameraUp, cameraFov, aspectRatio, 0.1f, 50.f);
+	renderer_ = renderer;
 
-	/*{
+	int w = 100, h = 50;
+	Button* btn1 = new Button(0, 0, w, h, []()->void {
+		Pipeline::getInstance().switchRendererMode();
+	}, "mode", renderer_);
+	button_.emplace_back(btn1);
+
+	Button* btn2 = new Button(0, h, w, h, []()->void {
+		Pipeline::getInstance().switchBackCulling();
+	}, "backCulling", renderer_);
+	button_.emplace_back(btn2);
+
+	Button* btn3 = new Button(0, 2 * h, w, h, []()->void {
+		Pipeline::getInstance().switchClipTriangle();
+	}, "clipTriangle", renderer_);
+	button_.emplace_back(btn3);
+
+	Button* btn4 = new Button(0, 3 * h, w, h, [&]()->void {
+		openLight_ = !openLight_;
+	}, "lightShader", renderer_);
+	button_.emplace_back(btn4);
+
+	{
 		Light* l = new Light();
 		l->position = Vec3f(5.f, 5.f, 5.f);
 		l->ambient = Vec3f(0.3f, 0.3f, 0.3f);
 		l->intensity = Vec3f(100.f, 100.f, 100.f);
 		l->p = 150;
-		light_.emplace_back(l);
-	}*/
+		std::vector<Light*> lp;
+		lp.emplace_back(l);
+		lightPool_.emplace_back(lp);
+	}
 
 	{
 		Light* l = new Light();
@@ -27,44 +52,53 @@ Scene::Scene(float aspectRatio) {
 		l->ambient = Vec3f(0.2f, 0.2f, 0.2f);
 		l->intensity = Vec3f(100.f, 100.f, 100.f);
 		l->p = 150;
-		light_.emplace_back(l);
+		std::vector<Light*> lp;
+		lp.emplace_back(l);
+		lightPool_.emplace_back(lp);
 	}
 
-//	{
-//		Light* l1 = new Light();
-//		l1->position = Vec3f(20.f, 10.f, 10.f);
-//		l1->ambient = Vec3f(0.1.f, 0.1.f, 0.1.f);
-//		l1->intensity = Vec3f(500.f, 500.f, 500.f);
-//		l1->p = 150;
-//		light_.emplace_back(l1);
-//
-//		Light* l2 = new Light();
-//		l2->position = Vec3f(-5.f, 25.f, 2.f);
-//		l2->ambient = Vec3f(0.1f, 0.1f, 0.1f);
-//		l2->intensity = Vec3f(500.f, 500.f, 500.f);
-//		l2->p = 150;
-//		light_.emplace_back(l2);
-//		l2->unOp = true;
-//	//}
+	{
+		std::vector<Light*> lp;
+		Light* l1 = new Light();
+		l1->position = Vec3f(20.f, 10.f, 10.f);
+		l1->ambient = Vec3f(0.1f, 0.1f, 0.1f);
+		l1->intensity = Vec3f(500.f, 500.f, 500.f);
+		l1->p = 150;
+		lp.emplace_back(l1);
+
+		Light* l2 = new Light();
+		l2->position = Vec3f(-5.f, 25.f, 2.f);
+		l2->ambient = Vec3f(0.1f, 0.1f, 0.1f);
+		l2->intensity = Vec3f(500.f, 500.f, 500.f);
+		l2->p = 150;
+		lp.emplace_back(l2);
+		l2->unOp = true;
+		lightPool_.emplace_back(lp);
+	}
 }
 
 Scene::~Scene() {
 	if (camera_ != nullptr)
 		delete camera_;
-	for (auto light : light_) {
-		delete light;
+	for (auto lights : lightPool_) {
+		for(auto light: lights)
+			delete light;
 	}
 	for (auto object : objects_) {
 		delete object;
 	}
+	for (auto btn : button_) {
+		delete btn;
+	}
 }
 
-void Scene::addModelObject(const char* filename, float scale) {
+void Scene::addModelObject(const char* filename, float scale, int lightIdx) {
 	ModelObject* object = new ModelObject();
 	object->addModel(filename);
 	object->setScale(scale);
 	object->setPos(Vec3f(0, 0, 0));
 	objects_.push_back(object);
+	lightIdx_ = lightIdx;
 }
 
 void Scene::addQuat(const char* filename) {
@@ -84,12 +118,22 @@ void Scene::addQuat(const char* filename) {
 
 
 void Scene::onRender() {
-	//renderTestShader();
-	renderLightShader();
+	if (openLight_) {
+		renderLightShader();
+	}
+	else {
+		renderTestShader();
+	}
+}
+
+void Scene::onRenderUI() {
+	for (auto btn : button_) {
+		btn->onRender(renderer_);
+	}
 }
 
 void Scene::renderTestShader() {
-	TestShader* shader = new TestShader();
+	TexShader* shader = new TexShader();
 	Pipeline::getInstance().bindShader(shader);
 	shader->viewMat4 = camera_->getViewMat4();
 	shader->projectMat4 = camera_->getProjectMat4();
@@ -106,7 +150,7 @@ void Scene::renderLightShader() {
 	Pipeline::getInstance().bindShader(shader);
 	shader->viewMat4 = camera_->getViewMat4();
 	shader->projectMat4 = camera_->getProjectMat4();
-	shader->light = light_;
+	shader->light = lightPool_[lightIdx_];
 	shader->viewPos = camera_->getEye();
 	for (auto obj : objects_) {
 		shader->modelMat4 = obj->getModelMat4();
@@ -115,11 +159,11 @@ void Scene::renderLightShader() {
 	}
 	delete shader;
 
-	TestShader* lshader = new TestShader();
+	WhiteShader* lshader = new WhiteShader();
 	Pipeline::getInstance().bindShader(lshader);
 	lshader->viewMat4 = camera_->getViewMat4();
 	lshader->projectMat4 = camera_->getProjectMat4();
-	for (auto l : light_) {
+	for (auto l : lightPool_[lightIdx_]) {
 		lshader->modelMat4 = l->getModelMat4();
 		shader->MVT = (shader->viewMat4 * shader->modelMat4).invert_transpose();
 		l->onRender();
@@ -156,14 +200,14 @@ void Scene::moveCameraFront(Vec3f offset) {
 }
 
 void Scene::leftLight() {
-	for (auto light : light_) {
+	for (auto light : lightPool_[lightIdx_]) {
 		if (light->unOp) continue;
 		light->leftPress();
 	}
 }
 
 void Scene::rightLight() {
-	for (auto light : light_) {
+	for (auto light : lightPool_[lightIdx_]) {
 		if (light->unOp) continue;
 		light->rightPress();
 	}
@@ -172,5 +216,14 @@ void Scene::rightLight() {
 void Scene::setObjectRotate(Vec3f direct, float angle) {
 	if (objects_.size() > 0) {
 		objects_[0]->setRotate(direct, angle);
+	}
+}
+
+void Scene::checkClickBtn(int x, int y, bool isPress) {
+	for (auto btn : button_) {
+		if (isPress)
+			btn->onPressDown(x, y);
+		else
+			btn->onPressUp();
 	}
 }
--- entity/Scene.h
+++ entity/Scene.h
@@ -3,14 +3,16 @@
 #include "ModelObject.h"
 #include <vector>
 #include "Light.h"
+#include "../ui/Button.h"
 class Scene
 {
 public:
-	Scene(float aspectRatio);
+	Scene(float aspectRatio, SDL_Renderer* renderer);
 	~Scene();
-	void addModelObject(const char* fileName, float scale = 1);
+	void addModelObject(const char* fileName, float scale = 1, int lightIdx = 0);
 	void addQuat(const char* fileName);
 	void onRender();
+	void onRenderUI();
 	void resetCamera();
 	void moveCamera(MoveDir dir);
 	void updateFront(int x, int y, bool init);
@@ -20,11 +22,17 @@ public:
 	void leftLight();
 	void rightLight();
 	void setObjectRotate(Vec3f direct, float angle);
+	void checkClickBtn(int x, int y, bool isPress);
 private:
 	void renderTestShader();
 	void renderLightShader();
 	Camera* camera_;
 	std::vector< Object*> objects_;
-	std::vector<Light*> light_;
+	SDL_Renderer* renderer_;
+	std::vector< Button*> button_;
+
+	std::vector<std::vector<Light*>> lightPool_;
+	bool openLight_ = true;
+	int lightIdx_;
 };
 
